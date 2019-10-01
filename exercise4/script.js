6d74a7295fde48b558076f40df151847b7370983
Finn Dobbs <finndobbs@ku.edu>
2019-10-01 17:25:09 -0500

    Add functionality to set border attributes

--- exercise4/script.js
+++ exercise4/script.js
@@ -1,1 +1,10 @@
-//this is js file
+function setBorder(){
+	let border = document.getElementById('paragraph')
+	let red = document.getElementById('bor_red').value;
+	let green = document.getElementById('bor_green').value;
+	let blue = document.getElementById('bor_blue').value;
+	let width = document.getElementById('bor_width').value;
+
+	border.style.borderColor = 'rgb('+red+', '+green+', '+blue+')';
+	border.style.borderWidth = width+'px';
+}
